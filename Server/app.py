efa6d7c139af81b2a10be47c209647427c8cf03c
BeatriceLeung <bbeatrice.leung@gmail.com>
2025-02-08 09:40:30 -0800

    Removed node_modules from topmost level

--- Server/app.py
+++ Server/app.py
@@ -22,7 +22,7 @@ KNOWN_FACES_DIR = './static/known_faces'
 RECVD_FACES_DIR = './static/recvd_faces'
 CSV_FILE = "voice_memos.csv"
 
-DEFAULT_LOCK_ID = "1"
+DEFAULT_LOCK_ID = "-1"
 
 
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
